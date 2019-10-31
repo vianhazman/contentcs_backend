52c332b25ee81bca2895a7a542ff409883ef8a89
vianhazman <vianhazman@gmail.com>
2019-10-31 14:55:58 +0700

    Add heroku.yml

--- contentcs_service/settings.py
+++ contentcs_service/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_ROOT = '/staticfiles'
 MEDIA_URL = '/src/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
