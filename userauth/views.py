f7cc5af789a8ad1489d339a1586a800d06ab18b1
vianhazman <vianhazman@gmail.com>
2019-11-15 16:49:59 +0700

    [DEV] allow foreign url redirect

--- userauth/views.py
+++ userauth/views.py
@@ -45,13 +45,14 @@ __all__ = ['LoginView', 'LogoutView', 'CallbackView']
 
 def clean_next_page(request, next_page):
     """
-    set settings.CAS_CHECK_NEXT to lambda _: True if you want to bypass this check.
+    buat cico: jangan lupa dinyalain lagi
     """
-    if not next_page:
-        return next_page
-    is_safe = getattr(settings, 'CAS_CHECK_NEXT', lambda _next_page: is_local_url(request.build_absolute_uri('/'), _next_page))
-    if not is_safe(next_page):
-        raise Exception("Non-local url is forbidden to be redirected to.")
+
+    # if not next_page:
+    #     return next_page
+    # is_safe = getattr(settings, 'CAS_CHECK_NEXT', lambda _next_page: is_local_url(request.build_absolute_uri('/'), _next_page))
+    # if not is_safe(next_page):
+    #     raise Exception("Non-loorig_iatcal url is forbidden to be redirected to.")
     return next_page
 
 
